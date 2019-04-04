f8d679684f104738fc179bfeac13fa558f186786
YulyKo <juliakotenko.forstudy@gmail.com>
2019-04-04 23:03:50 +0300

    created basis pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { GamePageComponent } from './pages/game-page/game-page.component';
+import { FirstPageComponent } from './pages/first-page/first-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminPageComponent
+    AdminPageComponent,
+    GamePageComponent,
+    FirstPageComponent
   ],
   imports: [
     BrowserModule,

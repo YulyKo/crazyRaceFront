97f2e4be2f5ef2aa7146896957bf376d65ef94d3
YulyKo <juliakotenko.forstudy@gmail.com>
2019-04-09 22:15:24 +0300

    created methods post, get for get categories/themes & random text. Todo for login

---
+++ src/app/service.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import {environment} from '../environments/environment';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceService {
+  private api = environment;
+
+  constructor( private http: HttpClient ) { }
+
+  postText( servers: object) {
+    return this.http.post( this.api.apiUrl + '/db/add/text', JSON.stringify(servers), {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })})
+      .subscribe(
+        (val) => {
+          console.log('POST call successful value returned in body', val);
+        },
+        response => {
+          console.log('POST call in error', response);
+        },
+        () => {
+          console.log('The POST observable is now completed.');
+        }
+      );
+  }
+
+  getCategoryAndTheme() {
+    return this.http.get(this.api.apiUrl + '/db/themes').toPromise();
+  }
+
+  getRandomText(category: string, theme: string) {
+    return this.http.get(this.api.apiUrl + '/db/text/?category=' + category + '&theme=' + theme).toPromise();
+  }
+
+}
+// TODO methods for login:
+//  - get - /db/user?login="значення"(повертає дані по користувачу)
+//  - post - /db/add/user - приймає JSON { "login": "...", ... }
+

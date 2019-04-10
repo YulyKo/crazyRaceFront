d4b0ed320069df4b9ebf4fb6be55e0e03deec479
YulyKo <juliakotenko.forstudy@gmail.com>
2019-04-10 18:37:13 +0300

    added checked text && some styles to .text

--- src/app/pages/game-page/game-page.component.ts
+++ src/app/pages/game-page/game-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-game-page',
@@ -6,10 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-page.component.css']
 })
 export class GamePageComponent implements OnInit {
+  text = 'To store dynamically-sized elements in Java, we used ArrayList. Whenever new elements are added to it, its increase their size automatically. ArrayList implements Java’s List Interface and part of Java’s Collection.';
+  indexLetter = 0;
+  indexSpan = 0;
+  letters = [{
+    symbol: this.text.split(''),
+    display: false,
+    correct: false
+  }];
+  typedLetter: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  @HostListener('document:keypress', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    this.typedLetter = event.key;
+    this.checkLetter();
   }
 
+  constructor() {
+  }
+
+  ngOnInit() {
+    for (let i = 0; i < this.letters[0].symbol.length; i++) {
+      const child = document.createElement('span');
+      child.innerText = this.letters[0].symbol[i];
+      document.getElementById('textZone').appendChild(child);
+      child.setAttribute('id', String(i));
+      child.setAttribute('class', 'text');
+    }
+  }
+
+  checkLetter() {
+    const span = document.getElementById(String(this.indexSpan));
+    if (this.letters[0].symbol[this.indexLetter] === this.typedLetter) {
+      this.letters[0].correct = true;
+      this.indexLetter++;
+      this.indexSpan++;
+      span.style.backgroundColor = 'rgb(13, 101, 12)';
+      span.style.display = 'none';
+    } else {
+      span.style.backgroundColor = '#8a0505';
+    }
+  }
 }
